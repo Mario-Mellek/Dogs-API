0f3dfa5898dd7acc62eac760174ca49d49b8d76e
Mario-Mellek <mario.samy266@gmail.com>
2022-12-08 17:40:39 +0200

    API changes

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 //Global variables
-const imageDiv= document.querySelector('img');
-const caption= document.querySelector('figcaption');
-const apiKey= '?api_key=demo';
-const url ='https://api.thedogapi.com/v1/images/search';
+const imageDiv = document.querySelector("img");
+const caption = document.querySelector("figcaption");
+const url = "https://dog.ceo/api/breeds/image/random";
 
 //fetching data using async await
 async function getData() {
-    const res= await fetch(url+ apiKey);
-    try {
-        const data= await res.json();
-        return data;
-    } catch (error) {
-        console.log(`something isn't right ${error}`);
-    }    
-};
+  const res = await fetch(url);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(`something isn't right ${error}`);
+  }
+}
 
 //space and touch listeners
-document.addEventListener('keypress',(e)=>{
-    if (e.code === 'Space'){
-        update();
-    };
+document.addEventListener("keypress", (e) => {
+  if (e.code === "Space") {
+    update();
+  }
 });
-document.addEventListener('touchstart',update)
+document.addEventListener("touchstart", update);
 
 //Updating the DOM after fetching the data
-function update(){
-getData().then(data=>{
-    imageDiv.src= data[0].url;
-    imageDiv.alt='Dog Picture';
-    if(data[0].breeds.length === 1){
-        const name= data[0].breeds[0].name;
-        caption.innerText=name;
-    }else{
-        caption.innerText='No Name available';
-    };
-});
-};
+function update() {
+  getData().then((data) => {
+    imageDiv.src = data.message;
+    imageDiv.alt = "Dog Picture";
+  });
+}
 
 update();
